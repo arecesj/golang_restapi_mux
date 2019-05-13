5594f8f8dfb3e9491323a23f10f351733145d7b5
Juan Areces <juan.areces@sendgrid.com>
2019-05-13 13:52:41 -0600

    Update and Delete methods

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // Create a New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var book Book
 
 	_ = json.NewDecoder(r.Body).Decode(&book)
@@ -61,11 +62,33 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 // Update a Single Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000))
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete a Single Book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 
 }
 
