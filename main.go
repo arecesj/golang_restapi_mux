85ae7cf398f56783e8619cac19c4bc78288f664a
Juan Areces <juan.areces@sendgrid.com>
2019-05-13 10:42:38 -0600

    Scaffolding for API

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Book Struct (model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create a New Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update a Single Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete a Single Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	// Init router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
